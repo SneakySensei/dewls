2e848dd9d98368fd7bb848ccd9b2be4fae74eccc
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-09-03 02:56:16 +0530

    feat(server): add rank to player game history

--- packages/backend/microservices/leaderboard/leaderboard.service.ts
+++ packages/backend/microservices/leaderboard/leaderboard.service.ts
@@ -16,3 +16,22 @@ export const fetchSeasonLeaderboard = async (
 
     return data;
 };
+
+export const fetchPlayerInSeasonLeaderboard = async (
+    player_id: MappedLeaderboard["player_id"],
+    season_id: MappedLeaderboard["season_id"],
+) => {
+    const { data, error } = await SupabaseService.getSupabase()
+        .from("leaderboard")
+        .select()
+        .eq("player_id", player_id!)
+        .eq("season_id", season_id!)
+        .single();
+
+    if (error) {
+        console.error(error);
+        throw error;
+    }
+
+    return data;
+};
--- packages/backend/microservices/player-game-history/player-game-history.service.ts
+++ packages/backend/microservices/player-game-history/player-game-history.service.ts
@@ -1,20 +1,32 @@
 import { SupabaseService } from "../../services";
-import { MappedPlayerGameHistory } from "../../utils/types/mappers.types";
+import {
+    MappedLeaderboard,
+    MappedPlayerGameHistory,
+} from "../../utils/types/mappers.types";
+import { fetchPlayerInSeasonLeaderboard } from "../leaderboard/leaderboard.service";
 
 export const fetchPlayerHistoryForSeason = async (
     player_id: MappedPlayerGameHistory["player_id"],
     season_id: MappedPlayerGameHistory["season_id"],
-) => {
-    const { data, error } = await SupabaseService.getSupabase()
-        .from("player_game_history")
-        .select()
-        .eq("player_id", player_id!)
-        .eq("season_id", season_id!);
+): Promise<{
+    player_game_history: MappedPlayerGameHistory[];
+    rank: MappedLeaderboard["rank"];
+}> => {
+    const { rank } = await fetchPlayerInSeasonLeaderboard(player_id, season_id);
+    const { data: historyData, error: historyError } =
+        await SupabaseService.getSupabase()
+            .from("player_game_history")
+            .select()
+            .eq("player_id", player_id!)
+            .eq("season_id", season_id!);
 
-    if (error) {
-        console.error(error);
-        throw error;
+    if (historyError) {
+        console.error(historyError);
+        throw historyError;
     }
 
-    return data;
+    return {
+        player_game_history: historyData,
+        rank,
+    };
 };
